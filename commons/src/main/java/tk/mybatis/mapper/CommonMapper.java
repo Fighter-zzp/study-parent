6710dc793b8bd627e6204a2e64130d202c2531b1
zzp <3387748327@qq.com>
2020-10-28 02:03:40 +0800

    创建统一依赖

---
+++ commons/src/main/java/tk/mybatis/mapper/CommonMapper.java
@@ -0,0 +1,8 @@
+package tk.mybatis.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+public interface CommonMapper<T> extends MySqlMapper<T>, Mapper<T> {
+}
+
